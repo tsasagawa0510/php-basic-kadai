d12087f0e6903fe1a1d491653995e30c960ad50e
tsasagawa0510 <mail@tsubasa-sasagawa.com>
2023-10-25 18:49:05 +0900

    add

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,25 @@
+<!DOCTYPE>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+<p>
+  <?php
+  // 連想配列を宣言・代入
+  $vegetables = [ 'name' => 'onion', 'price' => 200, 'weight' => 160 ];
+  
+  print_r($vegetables);
+
+
+
+
+  ?>
+</p>
+
+</body>
+
+</html>
